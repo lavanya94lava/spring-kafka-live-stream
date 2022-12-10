2f5db5879b4eb2ca24d9d14faee983a9cb25289e
lavanya singh <lavanyasingh94@github.com>
2022-12-11 02:31:43 +0530

    add kafka producer and handler for live stream

---
+++ src/test/java/com/lavanya/springkafkaproject/SpringKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanya.springkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
